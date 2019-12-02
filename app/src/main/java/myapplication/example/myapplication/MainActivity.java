e45bef8a377e9227cf0e7cb1ba8f62ce753a37a8
ogu22 <s.oguma22@gmail.com>
2019-12-02 15:47:42 +0900

    コメントアウト

--- app/src/main/java/myapplication/example/myapplication/MainActivity.java
+++ app/src/main/java/myapplication/example/myapplication/MainActivity.java
@@ -1,9 +1,6 @@
 package myapplication.example.myapplication;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,14 +11,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = findViewById(R.id.textView);
-        Button button = findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener(){
-
-            public void onClick(View v){
-                textView.setText("Hello world!");
-            }
-        });
+//        final TextView textView = findViewById(R.id.textView);
+//        Button button = findViewById(R.id.button);
+//
+//        button.setOnClickListener(new View.OnClickListener(){
+//
+//            public void onClick(View v){
+//                textView.setText("Hello world!");
+//            }
+//        });
+        //テスト
     }
 }
