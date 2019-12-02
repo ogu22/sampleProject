22d84ddbabc74a0c5015c56b3b62bc3cb46fb4bc
ogu22 <s.oguma22@gmail.com>
2019-12-02 15:33:12 +0900

    Initial commit

---
+++ app/src/main/java/myapplication/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package myapplication.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                textView.setText("Hello world!");
+            }
+        });
+    }
+}
